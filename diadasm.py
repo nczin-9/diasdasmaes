35f764c7eb0fda8a9b719893a5aa207724b84a5d
nczin-9 <00001093098582sp@al.educacao.sp.gov.br>
2025-05-05 13:11:05 -0300

    primeiro Commit

---
+++ diadasm.py
@@ -0,0 +1,29 @@
+import time
+
+
+mensagem =[
+   "feliz dias das Mães"
+   "💖💖💖💖💖💖💕💕💕💕❤️❤️❤️❤️❤️"
+   "Mãe, obrigado por tudo que a senhora fez e vem fazendo por mim.",
+   "Você é a melhor mãe do mundo!"
+   "❤️❤️💕💖😍😘🥰🥰🥰😍❤️❤️💕💖💖💕"
+   "Mamãe você é o amor",
+   "É o meu exemplo na vida!",
+   "Eu te ofereço um Dolly",
+   "Com toda emoção",
+   "feliz dias das Mães, Eu te Amo!"
+]
+
+for linha in mensagem:
+    print(linha)
+    time.sleep(1.5) 
+print()
+print("    ***   ***")   
+print("  ****** ******")
+print(" ***************")
+print("  ************")
+print("   **********")
+print("    ********")
+print("     *****")
+print("      ***")
+print("       *")
